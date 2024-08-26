e4637e52ca785ab2273d37ff207c71b92beef6ad
slamballais <lamballais.sander@gmail.com>
2024-08-26 21:22:59 +0200

    added fastorder testing

--- tests/testthat/test-fastorder.R
+++ tests/testthat/test-fastorder.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# verify that it works like sort( )
+set.seed(2000)
+x <- rnorm(1000)
+x2_short <- c(1, rep(0.05, 3), 0.01)
+x2_long <- c(1, rep(0.05, 15), 0.01)
+
+test_that("fastorder is identical to sort", {
+  expect_equal(sort(x, index.return = TRUE), fastorder(x))
+  expect_equal(sort(x2_short, index.return = TRUE), fastorder(x2_short))
+  expect_equal(sort(x2_long, index.return = TRUE), fastorder(x2_long))
 })
