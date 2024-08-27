afe8d627e3bafb9615f3bee1a38a55b11d4f5169
slamballais <lamballais.sander@gmail.com>
2024-08-27 14:50:27 +0200

    Removed commented test

--- tests/testthat/test-fastorder.R
+++ tests/testthat/test-fastorder.R
@@ -7,5 +7,4 @@ x2_long <- c(1, rep(0.05, 15), 0.01)
 test_that("fastorder is identical to sort", {
   expect_equal(sort(x, index.return = TRUE), fastorder(x))
   expect_equal(sort(x2_short, index.return = TRUE), fastorder(x2_short))
-  #expect_equal(sort(x2_long, index.return = TRUE), fastorder(x2_long))
 })
