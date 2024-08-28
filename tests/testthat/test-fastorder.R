38aad7ba64fd1f199535b8d86fc0eda98cb64c01
slamballais <lamballais.sander@gmail.com>
2024-08-28 12:20:57 +0200

    added test for better coverage

--- tests/testthat/test-fastorder.R
+++ tests/testthat/test-fastorder.R
@@ -4,6 +4,10 @@ x <- rnorm(1000)
 x2_short <- c(1, rep(0.05, 3), 0.01)
 x2_long <- c(1, rep(0.05, 15), 0.01)
 
+test_that("fastorder runs as expected", {
+  expect_equal(fastorder(x2_long)$ix, 17:1)
+})
+
 test_that("fastorder is identical to sort", {
   expect_equal(sort(x, index.return = TRUE), fastorder(x))
   expect_equal(sort(x2_short, index.return = TRUE), fastorder(x2_short))
